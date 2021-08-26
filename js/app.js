92bca8d0bbfdbc203fd85e6d82dd7209493a2e45
IslamHeza <islamo.heza@gmail.com>
2021-08-26 19:28:09 +0200

    fix bugs

--- js/app.js
+++ js/app.js
@@ -29,14 +29,15 @@ const topBtn = document.getElementById('top');
 //adding anchers as items to the list menue 
 function createMenuItem(target, sectionTitle) {
     menuItem = document.createElement('li');
-    menuItem.innerHTML = `<a class="menu__link" href="#${target}">${sectionTitle}</li>`;
+    menuItem.innerHTML = `<a class="menu__link" data-nav=${target}  href="#${target}">${sectionTitle}</li>`;
     menu.appendChild(menuItem);
 }
 
 //check for the active section coordinates
 function viewPort(section) {
-    let position = section.getBoundingClientRect();
-    return position.top >= 0;
+    if(section.getBoundingClientRect().top >= -400 && section.getBoundingClientRect().top <= 220){
+        return  true;
+    }
 }
 
 /**
@@ -61,13 +62,18 @@ buildNav();
 // Set sections as active 
 function activeView() {
     sections.forEach((section) => {
+        let activeSection = menu.querySelector(`[data-nav=${section.id}]`);
+        menu.querySelector(`[data-nav=${section.id}]`).classList
         if (viewPort(section)) {
-            if (!section.classList.contains('your-active-class')) {
+            if (!section.classList.contains('active')) {
                 // Add class 'active' to section when near top of viewport
-                section.classList.add('your-active-class');
+                section.classList.add('active');
+                activeSection.classList.add('activeLink');
             }
         } else {
-            section.classList.remove('your-active-class');
+            section.classList.remove('active');
+            activeSection.classList.remove('activeLink');
+
         }
     });
 }
@@ -75,3 +81,13 @@ function activeView() {
 
 // change the style of active section while scrolling 
 document.addEventListener('scroll', activeView);
+
+//scroll into the target section after click on it's address
+menu.addEventListener("click", (event) => {
+    event.preventDefault();
+    document
+      .getElementById(`${event.target.dataset.nav}`)
+      .scrollIntoView({inline: "nearest" , block: "end"});
+      console.log('scroll');
+  });
+  
